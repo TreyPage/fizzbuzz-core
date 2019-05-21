0b518545f0b465970e83980824bd2084cb27a793
Terrell Page <tpage46@gmail.com>
2019-05-21 16:03:44 -0600

    Initial commit

---
+++ test/edu/cnm/deepdive/FizzBuzzTest.java
@@ -0,0 +1,42 @@
+package edu.cnm.deepdive;
+
+import static edu.cnm.deepdive.FizzBuzz.BUZZ;
+import static edu.cnm.deepdive.FizzBuzz.FIZZ;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+  public static final String FB = FIZZ + BUZZ;
+
+  @Test
+  void fizzBuzzValue() {
+    String actual;
+    actual = FizzBuzz.fizzBuzzValue(3);
+    assertEquals(FIZZ, actual);
+    actual = FizzBuzz.fizzBuzzValue(99);
+    assertEquals(FIZZ, actual);
+
+    actual = FizzBuzz.fizzBuzzValue(5);
+    assertEquals(BUZZ, actual);
+    actual = FizzBuzz.fizzBuzzValue(100);
+    assertEquals(BUZZ, actual);
+
+    actual = FizzBuzz.fizzBuzzValue(2);
+    assertEquals("2", actual);
+    actual = FizzBuzz.fizzBuzzValue(11);
+    assertEquals("11", actual);
+    actual = FizzBuzz.fizzBuzzValue(101);
+    assertEquals("101", actual);
+
+    actual = FizzBuzz.fizzBuzzValue(120);
+    assertEquals(FB, actual);
+    actual = FizzBuzz.fizzBuzzValue(15);
+    assertEquals(FB, actual);
+
+    assertThrows(IllegalArgumentException.class, () -> FizzBuzz.fizzBuzzValue(0));
+    assertThrows(IllegalArgumentException.class, () -> FizzBuzz.fizzBuzzValue(-15));
+
+  }
+}
