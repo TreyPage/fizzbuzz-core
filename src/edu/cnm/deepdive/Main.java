bacf03d03612903debce3a859bd9313c4371ad01
Terrell Page <tpage46@gmail.com>
2019-05-22 14:23:43 -0600

    Created JavaDocs

--- src/edu/cnm/deepdive/Main.java
+++ src/edu/cnm/deepdive/Main.java
@@ -1,7 +1,19 @@
 package edu.cnm.deepdive;
 
+/**
+ * Exercises the methods of the {@link FizzBuzz} class.
+ *
+ * @author Trey Page &amp; Deep Dive Coding Java+Android Cohort 7.
+ * @version 1.0.0
+ */
 public class Main {
 
+  private Main() {}
+
+  /**
+   * Method runs a loop 1&ndash;100 and prints a {@link String} based on the {@link FizzBuzz} class.
+   * @param args Command line arguments (ignored).
+   */
   public static void main(String[] args) {
     for(int i = 1; i <= 100; i++) {
       System.out.println(FizzBuzz.fizzBuzzValue(i));
